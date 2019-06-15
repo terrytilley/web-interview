b5599773b496a96433f61b64a57174a042a26d28
Terry Tilley <terry@terrytilley.com>
2019-06-15 18:37:47 +0100

    styled Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,7 +6,13 @@ import logo from '../../images/logo.png'
 function Header() {
   return (
     <div className="app-header">
+      <button className="menu-toggle">
+        <span />
+        <span />
+        <span />
+      </button>
       <img src={logo} className="app-logo" alt="Babylon Health" />
+      <div className="user-icon">NU</div>
     </div>
   )
 }
